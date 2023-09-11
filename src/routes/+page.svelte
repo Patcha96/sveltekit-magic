96d4bde44a2a9889a2b21a784db87aa1420c7b69
Patcha96 <eryk.grzeskow@gmail.com>
2023-09-11 12:55:32 +0800

    basic routing, layouts, dummy data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<h1 class="text-green-300">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-<button class="btn btn-primary">Button</button>
+<script lang="ts">
+</script>
+
+<a
+  href="/login"
+  class="btn btn-primary flex h-screen justify-center content-center"
+  >Get Started</a
+>
